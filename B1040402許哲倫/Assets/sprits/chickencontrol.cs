0c47b199ddab79d4dd6da6694632b62c40f85846
james890311 <55519698+james890311@users.noreply.github.com>
2019-10-24 11:57:47 +0800

    雞雞

---
+++ B1040402許哲倫/Assets/sprits/chickencontrol.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class chickencontrol : MonoBehaviour
+{
+    private Animator ani;
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            ani.SetTrigger("吃飯觸發器");
+        }
+        if(Input.GetKey(KeyCode.R))
+        {
+            ani.SetBool("跑步開關", true);
+        }
+        else
+        {
+            ani.SetBool("跑步開關", false);
+        }
+    }
+}
